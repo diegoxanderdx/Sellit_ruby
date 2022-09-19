f340fc5b75524438cfab5dd64e8e48e446b5e2df
Diegomez-dev <diegoagc@yahoo.es>
2022-09-18 22:32:14 -0500

    New product form, create product and Model validations

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -16,4 +16,35 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_select '.description', 'PS4 en buen estado'
     assert_select '.price', '150$'
   end
+
+  test 'renders a new product form' do
+    get new_product_path
+
+    assert_response  :success
+    assert_select 'form'
+  end
+
+  test 'allows to create a new product' do
+    post products_path, params: {
+      product:{
+        title: 'redmi',
+        description: 'Sin cargador',
+        price: 45
+      }
+    }
+
+    assert_redirected_to products_path
+  end
+  
+  test 'doesnt allow to create a new product with empty fields' do
+    post products_path, params: {
+      product:{
+        title: 'redmi',
+        description: '',
+        price: 45
+      }
+    }
+
+    assert_response :unprocessable_entity
+  end
 end
